4edc62816d96661c67186b301faba62fc4400511
mahmoudJ10 <mahmoudjaber11222@gmail.com>
2026-09-10 12:59:55 +0300

    feat: mount subscription routes

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import promoRouter from "./modules/subscription/routes.js";
 const app = express();
 
 // global middlewares
+app.use(express.json());
+app.use("/subscription", promoRouter.router);
 
 app.get("/", (_req, res) => {
   res.json({ message: "API is running..." });
