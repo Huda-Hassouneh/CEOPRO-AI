399cdb6fe484d0aac77036d4110ff26b91cb1559
mahmoudJ10 <mahmoudjaber11222@gmail.com>
2026-09-10 12:51:24 +0300

    feat: standardize validation and error handling

---
+++ src/backend/src/validators/validateBody.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+import { ERROR_CODES } from "../errors/error-codes.js";
+import { ERROR_DEFINITIONS } from "../errors/error-defentions.js";
+import { errorResponse } from "../types/response.js";
+
+export function validateBody(schema: z.ZodType) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      const error = ERROR_DEFINITIONS[ERROR_CODES.VALIDATION_ERROR];
+
+      return res
+        .status(error.statusCode)
+        .json(
+          errorResponse(
+            error.message,
+            ERROR_DEFINITIONS[ERROR_CODES.VALIDATION_ERROR].statusCode,
+            ERROR_CODES.VALIDATION_ERROR,
+            result.error.flatten()
+          )
+        );
+    }
+
+    req.body = result.data;
+    next();
+  };
+}
